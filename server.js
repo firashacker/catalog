b866186f4f3a5e28d92ef9db8437563a6c18dfe7
firashacker <firastom0@gmail.com>
2025-07-26 03:38:20 +0300

    preparing to host, seperating frontemd from backend

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 9000;
+const port = process.env.SERVER_PORT || 3000; // Use environment variable or default to 3000
 const publicDirs = ["uploaded"];
 const mainIndex = `${publicDirs[1]}/index.html`;
 const bodyParser = require("body-parser");
